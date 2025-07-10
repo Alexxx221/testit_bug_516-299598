264135c46d8cc7c493ed1234df48103101c25681
meshkov_as <meshkov_as@magnit.ru>
2025-07-10 14:19:37 +0300

    убрал @title

--- src/test/java/test/TestA.java
+++ src/test/java/test/TestA.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.testit.annotations.ExternalId;
-import ru.testit.annotations.Title;
 import ru.testit.annotations.WorkItemIds;
 
 public class TestA {
 
     @BeforeEach
-    @Title("1`23")
     void beforeEach() {
         Assertions.assertTrue(false, "beforeEach c exception из класса A");
     }
